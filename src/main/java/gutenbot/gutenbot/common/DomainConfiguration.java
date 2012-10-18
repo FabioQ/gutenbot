06ba762f208596f9de078f927bbf7f32dba4285b
Federico Roffi <fedoverseas@gmail.com>
2012-10-19 00:05:18 +0200

    Configurations loading completed.

--- src/main/java/gutenbot/gutenbot/common/DomainConfiguration.java
+++ src/main/java/gutenbot/gutenbot/common/DomainConfiguration.java
@@ -1,30 +1,49 @@
 package gutenbot.gutenbot.common;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 public class DomainConfiguration {
-	
+
 	Logger logger = Logger.getLogger(DomainConfiguration.class);
 	File configurationFile;
+	LinkedList<String> urls = new LinkedList<String>();
 	boolean valid;
-	
+
 	public DomainConfiguration(File configurationFile) {
-		
-		logger.info("loading configuration from: "+configurationFile.getName());
+
+		logger.info("loading configuration from: " + configurationFile.getName());
 
 		this.configurationFile = configurationFile;
-		
-		valid = true;
+
+		SAXReader reader = new SAXReader();
+		try {
+			Document document = reader.read(new FileInputStream(configurationFile));
+			List list = document.selectNodes("/feeds/url");
+			for(Object o:list){
+				Element url = (Element) o;
+				urls.add(url.getText());
+			}
+			valid = true;
+		} catch (Exception e) {
+			valid = false;
+			logger.error("Error parsing configuration file", e);
+		}
+
 	}
-	
+
 	public LinkedList<String> getRssURLs() {
-		return null;
+		return urls;
 	}
-	
-	public String getName(){
+
+	public String getName() {
 		return configurationFile.getName();
 	}
 
