ecaa9765e4ca971850d6e14209454198cfba93f9
Fabio Quinzi <fabio.quinzi@gmail.com>
2012-10-23 15:53:41 +0200

    Fixed all bugs in DispatcherTest.java. Now it perfectly works.

--- src/test/java/gutenbot/gutenbot/DispatcherTest.java
+++ src/test/java/gutenbot/gutenbot/DispatcherTest.java
@@ -12,20 +12,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndEntryImpl;
 
 public class DispatcherTest {
 
 	@Test
-	public void test() {
+	public void dispatcherTest() {
 		//initialization phase
 		Destination destination = null;
 		String feedURL = "http://www.gazzetta.it/rss/Calcio.xml";
 		LinkedList<SyndEntry> newsList = CreateSyndEntry();
 		System.out.println("test dispatcher");
 		Feed feedObj = new Feed(feedURL, destination, newsList);
+		System.out.println("Stampo il titolo del primo elemento della lista di feed per testare la creazione dell'oggetto Feed:");
+		System.out.println(feedObj.getSyndEntryList().get(0).getTitle());
 		//TODO parser selection phase
 		Dispatcher dispatcher = new Dispatcher(feedObj);
-		System.out.println("Test Ended without errors");
 	}
 
 	private LinkedList<SyndEntry> CreateSyndEntry(){
@@ -33,15 +35,15 @@ public class DispatcherTest {
 		enclosuresA.add("http://images2.gazzettaobjects.it/Media/Foto/2012/10/20/combo_365--630x365.jpg");
 		List enclosuresB = new ArrayList();
 		enclosuresB.add("http://images2.gazzettaobjects.it/Media/Foto/2012/10/20/LAPR0734--630x365.JPG");
-		SyndEntry newsA = null;
-		SyndEntry newsB = null;
+		SyndEntry newsA = new SyndEntryImpl();
+		SyndEntry newsB = new SyndEntryImpl();
 		newsA.setTitle("Juve-Napoli, sfida totale Tra rivincite e sospetti");
 		newsA.setLink("http://www.gazzetta.it/Calcio/Squadre/Juventus/20-10-2012/juve-napoli-sfida-totale-rivincite-sospetti-912961817332.shtml");
 		newsA.setEnclosures(enclosuresA);
 		newsB.setTitle("Bonucci reagisce a una rapina Un pugno e l'aggressore fugge");
 		newsB.setLink("http://www.gazzetta.it/Calcio/Squadre/Juventus/20-10-2012/bonucci-aggredito-reagisce-pugno-rapinatore-che-scappa-912961418231.shtml");
 		newsA.setEnclosures(enclosuresB);
-		LinkedList<SyndEntry> newsList = null;
+		LinkedList<SyndEntry> newsList = new LinkedList<SyndEntry>();
 		newsList.add(newsA);
 		newsList.add(newsB);
 		return newsList;	
