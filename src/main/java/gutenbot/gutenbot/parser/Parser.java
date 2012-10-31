35b994b1943492bbdfcf3980e2eea3a0d0fa22d6
Fabio Quinzi <fabio.quinzi@gmail.com>
2012-10-31 11:59:42 +0100

    errors on annotation fixed

--- src/main/java/gutenbot/gutenbot/parser/Parser.java
+++ src/main/java/gutenbot/gutenbot/parser/Parser.java
@@ -1,15 +1,9 @@
 package gutenbot.gutenbot.parser;
 
-import java.net.URISyntaxException;
-
 import gutenbot.gutenbot.dto.Article;
-import gutenbot.gutenbot.dto.Feed;
-import gutenbot.gutenbot.dispatcher.Dispatcher;
 
-public class Parser {
+public interface Parser {
 	
-	public Parser(Feed feed, String domain) throws URISyntaxException{
-		
-	}
+	Article parse();
 
 }
