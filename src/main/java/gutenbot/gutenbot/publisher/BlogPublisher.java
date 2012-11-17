85674c4072ba485217be19525fcadc6e2d58c773
Fabio Quinzi <fabio.quinzi@gmail.com>
2012-11-17 19:56:44 +0100

    Working publisher post now on wordpress with no problem of 1.7 version

--- src/main/java/gutenbot/gutenbot/publisher/BlogPublisher.java
+++ src/main/java/gutenbot/gutenbot/publisher/BlogPublisher.java
@@ -12,8 +12,8 @@ public class BlogPublisher {
 	public static void main(String[] args) throws MalformedURLException, XmlRpcFault {
 		Wordpress WP = new Wordpress("admin", "alonso11", "http://www.sportsponsorizzazioni.com/xmlrpc.php");
 	    Page post = new Page();
-	    post.setTitle("titolo prova");
-	    post.setDescription("prova post");
+	    post.setTitle("GODO");
+	    post.setDescription("PORCO DI DIO 1.7");
 	    String newPostIds = WP.newPost(post, true);
 	    int newPostId = Integer.valueOf(newPostIds).intValue();
 	    Page postNow = WP.getPost(newPostId);
