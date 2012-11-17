b22672fce3d3dfebb1cb567904310b30378c71e8
Federico Roffi <fedoverseas@gmail.com>
2012-11-17 21:02:39 +0100

    Added Saxon Parser

---
+++ src/main/java/gutenbot/gutenbot/parser/SaxonParser.java
@@ -0,0 +1,72 @@
+package gutenbot.gutenbot.parser;
+
+import gutenbot.gutenbot.dto.Article;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+import org.jdom.transform.XSLTransformer;
+
+public class SaxonParser implements Parser {
+
+	Logger logger = Logger.getLogger(SaxonParser.class);
+
+	@Override
+	public Article parse(String articleUrl) {
+		System.out.println("start");
+		try {
+
+			System.out.println(articleUrl);
+			URL url1 = new URL(articleUrl);
+			HttpURLConnection conn = (HttpURLConnection) url1.openConnection();
+			conn.addRequestProperty("User-Agent", "Mozilla/5.0 " + "(Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) " + "Gecko/20061204 Firefox/2.0.0.1");
+			// build a JDOM tree from a SAX stream provided by tagsoup
+			SAXBuilder builder = new SAXBuilder("org.ccil.cowan.tagsoup.Parser");
+			System.out.println("caricamento html dal sito");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			Document doc;
+			System.out.println("creazione del documento");
+			doc = builder.build(reader);
+
+			XMLOutputter outputter = new XMLOutputter();
+//			try {
+//				outputter.output(doc, System.out);
+//			} catch (IOException e) {
+//				System.err.println(e);
+//			}
+
+			System.out.println("carico il transformer");
+			XSLTransformer transformer = new XSLTransformer("resources/test.xsl");
+			System.out.println("trasformo");
+
+			Document out = transformer.transform(doc);
+
+			try {
+				outputter.output(out, System.out);
+			} catch (IOException e) {
+				System.err.println(e);
+			}
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
--- src/test/java/gutenbot/gutenbot/ParserTest.java
+++ src/test/java/gutenbot/gutenbot/ParserTest.java
@@ -1,6 +1,7 @@
 package gutenbot.gutenbot;
 
 import gutenbot.gutenbot.parser.Parser;
+import gutenbot.gutenbot.parser.SaxonParser;
 
 import java.io.IOException;
 
@@ -15,6 +16,12 @@ public class ParserTest {
 		FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("resources/applicationContext.xml");
 		Parser gazzetta = (Parser) ctx.getBean("gazzetta.it");
 		gazzetta.parse("http://www.gazzetta.it/Calcio/Squadre/Juventus/20-10-2012/juve-napoli-sfida-totale-rivincite-sospetti-912961817332.shtml");
+	}
+	
+	@Test
+	public void testSaxon() throws IOException {
+		SaxonParser p = new SaxonParser();
+		p.parse("http://www.corriere.it/economia/12_novembre_11/romney-facebook-fan_b776cb92-2bfa-11e2-a3f0-bca5bc7cc62d.shtml");
 	}
 
 }
