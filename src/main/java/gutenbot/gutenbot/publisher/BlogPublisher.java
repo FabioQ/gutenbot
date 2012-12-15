7b1a0b0c0f90bd14b530b0cbea331f093fe3bce0
Fabio Quinzi <fabio.quinzi@gmail.com>
2012-12-15 11:19:13 +0100

    not sure of what I've done with this update

--- src/main/java/gutenbot/gutenbot/publisher/BlogPublisher.java
+++ src/main/java/gutenbot/gutenbot/publisher/BlogPublisher.java
@@ -27,14 +27,14 @@ public class BlogPublisher {
 	public void blogPublish(String articleTitle, String articleText, String sourceDomain, String sourceAuthor, String blogCategory) throws XmlRpcFault{
 		String fullArticleContent = articleText + "<br><br><br>- Author: " + sourceAuthor + "<br>- Source: <a href=\"http://" + sourceDomain + "\" rel=\"nofollow\">"+ sourceDomain +"</a>" ;
 		//TODO use the category arg to create the XMLRPC array and select categories
-		//DONE add tags (words with 5 or more characters in the title)  -> there is no tag methos, so it will be done with an automatic tag system plugin.
+		//add tags - there is no tag methos, so it will be done with an automatic tag system plugin.
 		this.post = new Page();
 	    this.post.setTitle(articleTitle);
 	    this.post.setDescription(fullArticleContent);
 	    String newPostIds = this.WP.newPost(post, true);
 	    int newPostId = Integer.valueOf(newPostIds).intValue();
 	    Page postNow = WP.getPost(newPostId);
-		System.out.println("Article Posted.");
+		System.out.println("Article Posted.  Title=> "+ articleTitle);
 	}
 
 }
