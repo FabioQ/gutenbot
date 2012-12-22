8afc0d2063c9f98cdfe26accf0495ca48ba4062e
Fabio Quinzi <fabio.quinzi@gmail.com>
2012-12-22 15:23:26 +0100

    Added a publishingDate clone feature, now our articles are published on the same date of the article parsed, that's pretty good to mix all articles together and avoid to let somone thiks that it's an automatic work.

--- src/main/java/gutenbot/gutenbot/publisher/BlogPublisher.java
+++ src/main/java/gutenbot/gutenbot/publisher/BlogPublisher.java
@@ -12,6 +12,8 @@ import redstone.xmlrpc.XmlRpcFault;
 import net.bican.wordpress.Page;
 import net.bican.wordpress.Wordpress;
 
+import java.util.Date;
+
 public class BlogPublisher {
 
 	Logger logger = Logger.getLogger(BlogPublisher.class);
@@ -24,13 +26,14 @@ public class BlogPublisher {
 	    logger.debug("Connected to blog.");
 	}
 	
-	public void blogPublish(String articleTitle, String articleText, String sourceDomain, String sourceAuthor, String blogCategory) throws XmlRpcFault{
+	public void blogPublish(String articleTitle, String articleText, Date pubDate, String sourceDomain, String sourceAuthor, String blogCategory) throws XmlRpcFault{
 		String fullArticleContent = articleText + "<br><br><br>- Author: " + sourceAuthor + "<br>- Source: <a href=\"http://" + sourceDomain + "\" rel=\"nofollow\">"+ sourceDomain +"</a>" ;
 		//TODO use the category arg to create the XMLRPC array and select categories
 		//add tags - there is no tag methos, so it will be done with an automatic tag system plugin.
 		this.post = new Page();
 	    this.post.setTitle(articleTitle);
 	    this.post.setDescription(fullArticleContent);
+	    this.post.setDateCreated(pubDate);
 	    String newPostIds = this.WP.newPost(post, true);
 	    int newPostId = Integer.valueOf(newPostIds).intValue();
 	    Page postNow = WP.getPost(newPostId);
